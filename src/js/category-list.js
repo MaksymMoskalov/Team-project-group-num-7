575b4289024ab756b650f5d0f7357f118424fae8
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-09-03 17:54:38 +0200

    firts version
    получаем список с категориями

---
+++ src/js/category-list.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const getCategoryList = document.querySelector(".category-list");
+
+function createList(array) {
+    return array.map(
+        ({ list_name }) => `<li class="category-item">${list_name}</li>`
+    ).join('');
+}
+
+async function categoryList() {
+    const category_url = 'https://books-backend.p.goit.global/books/category-list';
+    const list_data = await axios.get(category_url);
+    const markup = createList(list_data.data);
+    getCategoryList.insertAdjacentHTML('beforeend', markup);
+    return list_data.data;
+}
+
+categoryList();
