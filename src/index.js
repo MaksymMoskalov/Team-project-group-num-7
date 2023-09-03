1c488d8aaf8d5aa1794f9e0babc06963d29173e2
Maksim <max.godnavi133@gmail.com>
2023-09-03 11:26:28 +0300

    Rest to modal

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// В папці js створюємо файлик в якому бедете працювати та імпортуєте його сюди
+import './js/modal.js';
+import './js/book-list.js';
+import './js/mobile-menu.js';
+import './js/scroll-up.js';
+import './js/modal-window-info-book.js';
---
+++ src/js/book-list.js
@@ -0,0 +1,68 @@
+import axios from 'axios';
+
+const ulGlobal = document.querySelector('.ul-global');
+
+async function axiosPosts() {
+  const URL = 'https://books-backend.p.goit.global/books/top-books';
+  const { data } = await axios.get(URL);
+  console.log('data:', data);
+  workWithData({ data });
+}
+axiosPosts();
+
+function workWithData({ data }) {
+  try {
+    data.forEach(category => {
+      const { list_name, books } = category;
+      console.log('books', books);
+      if (books.length > 0) {
+        const liGlobal = document.createElement('li');
+        ulGlobal.appendChild(liGlobal);
+
+        const categoryPar = document.createElement('p');
+        categoryPar.classList.add('categories');
+        categoryPar.textContent = `${list_name}`;
+        liGlobal.appendChild(categoryPar);
+
+        const categoryList = document.createElement('ul');
+        categoryList.classList.add('ul-category');
+        liGlobal.appendChild(categoryList);
+
+        categoryList.insertAdjacentHTML(
+          'beforeend',
+          createBookListMarkup({ books })
+        );
+        const seeMore = document.createElement('button');
+        seeMore.textContent = 'see more';
+        seeMore.classList.add('see-more');
+        liGlobal.appendChild(seeMore);
+        seeMore.addEventListener('click', () => {
+          const categoryBooks = categoryList.querySelectorAll('.book');
+          categoryBooks.forEach(book => {
+            book.style.display = 'block';
+          });
+          seeMore.style.display = 'none';
+        });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function createBookListMarkup({ books }) {
+  const markup = books
+    .map(({ list_name, book_image, title, author, _id }) => {
+      return `<li class="book" data-id="${_id}">
+      <div class='box'>
+        <img src="${book_image}" alt="${list_name}" />
+      </div>
+      <h3 class="title-main">${title}</h3>
+      <p class="author-main">${author}</p>
+    </li>`;
+    })
+    .join('');
+  return markup;
+}
+
+function seeMore() {}
