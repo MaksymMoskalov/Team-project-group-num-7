d67423dd71cdb929f8964be711a0c0a6ffce70bf
makssozykin <mvsozykin@gmail.com>
2023-09-04 23:42:47 +0300

    Shopping list finish

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -69,8 +69,8 @@ function createBookListMarkUp(arr) {
                 </div>
               </div>
               <button type="button" class="btn-delete" data-id="${_id}">
-                <svg class="icon-delete" width=16 height=16>
-                    <use href="./images/header-imgs/header-sprite.svg#icon-trash"></use>
+                <svg class="icon-delete" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                     <path d="M8.25 2.75H13.75M2.75 5.5H19.25M17.4167 5.5L16.7738 15.1427C16.6774 16.5894 16.6291 17.3128 16.3167 17.8613C16.0416 18.3441 15.6266 18.7323 15.1265 18.9747C14.5585 19.25 13.8335 19.25 12.3836 19.25H9.61643C8.1665 19.25 7.44153 19.25 6.87348 18.9747C6.37336 18.7323 5.95841 18.3441 5.68332 17.8613C5.37085 17.3128 5.32263 16.5894 5.22618 15.1427L4.58333 5.5M9.16667 9.625V14.2083M12.8333 9.625V14.2083" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
               </button>
             </li>`;
@@ -78,7 +78,6 @@ function createBookListMarkUp(arr) {
     .join('');
 }
 ulList.addEventListener('click', deleteBtn);
-const btnDelete = document.querySelector('.btn-delete');
 
 
 // Delete from LocalStorage and fron Shopping List
