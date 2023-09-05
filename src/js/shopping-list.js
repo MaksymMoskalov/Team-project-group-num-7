a5d81e59e4b249eed1fc24c0b60eca883031d886
makssozykin <mvsozykin@gmail.com>
2023-09-05 21:45:37 +0300

    button, svg are working, media 320px ok

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -80,8 +80,8 @@ function createBookListMarkUp(arr) {
                 </div>
               </div>
               <button type="button" class="btn-delete" data-id="${_id}">
-                <svg class="icon-delete" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
-                     <path d="M8.25 2.75H13.75M2.75 5.5H19.25M17.4167 5.5L16.7738 15.1427C16.6774 16.5894 16.6291 17.3128 16.3167 17.8613C16.0416 18.3441 15.6266 18.7323 15.1265 18.9747C14.5585 19.25 13.8335 19.25 12.3836 19.25H9.61643C8.1665 19.25 7.44153 19.25 6.87348 18.9747C6.37336 18.7323 5.95841 18.3441 5.68332 17.8613C5.37085 17.3128 5.32263 16.5894 5.22618 15.1427L4.58333 5.5M9.16667 9.625V14.2083M12.8333 9.625V14.2083" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <svg class="icon-delete" data-id="${_id}" xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                     <path data-id="${_id}" d="M8.25 2.75H13.75M2.75 5.5H19.25M17.4167 5.5L16.7738 15.1427C16.6774 16.5894 16.6291 17.3128 16.3167 17.8613C16.0416 18.3441 15.6266 18.7323 15.1265 18.9747C14.5585 19.25 13.8335 19.25 12.3836 19.25H9.61643C8.1665 19.25 7.44153 19.25 6.87348 18.9747C6.37336 18.7323 5.95841 18.3441 5.68332 17.8613C5.37085 17.3128 5.32263 16.5894 5.22618 15.1427L4.58333 5.5M9.16667 9.625V14.2083M12.8333 9.625V14.2083" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
               </button>
             </li>`;
@@ -91,13 +91,14 @@ function createBookListMarkUp(arr) {
 }
 ulList.addEventListener('click', deleteBtn);
 
-// Delete from LocalStorage and fron Shopping List
+// Delete from LocalStorage and from Shopping List
 function deleteBtn(event) {
-  if (event.target.nodeName !== 'BUTTON') {
+  if (event.target.nodeName !== 'BUTTON' && event.target.nodeName !== 'svg' && event.target.nodeName !== 'path' && '#text') {
+    console.dir(event.target);
     return;
   }
   const data = getLocalData();
-  console.log(data);
+  
   const idx = data.findIndex(({ _id }) => _id === event.target.dataset.id);
   data.splice(idx, 1);
   savedData(data);
