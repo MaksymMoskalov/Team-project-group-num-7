64e9c59313216f87bdbbb2ba0d26a6d0b1fb07b0
KovalenkoSerhiy <118220875+KovalenkoSerhiy@users.noreply.github.com>
2023-09-05 23:08:29 +0300

    rebild

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,21 +1,41 @@
-// (() => {
-//   const mobileMenu = document.querySelector('.js-menu-container');
-//   const openMenuBtn = document.querySelector('.js-open-menu');
-//   const closeMenuBtn = document.querySelector('.js-close-menu');
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const modalBackdrop = document.querySelector('.modal-backdrop');
+  const body = document.body;
 
-//   const toggleMenu = () => {
-//     const isMenuOpen =
-//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-//     mobileMenu.classList.toggle('is-open');
-//   };
+  const lockScroll = () => {
+    body.style.overflow = 'hidden';
+  };
 
-//   openMenuBtn.addEventListener('click', toggleMenu);
+  const unlockScroll = () => {
+    body.style.overflow = '';
+  };
 
-//   // Close the mobile menu on wider screens if the device orientation changes
-//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-//     if (!e.matches) return;
-//     mobileMenu.classList.remove('is-open');
-//     openMenuBtn.setAttribute('aria-expanded', false);
-//   });
-// })();
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    if (!isMenuOpen) {
+        lockScroll(); 
+        modalBackdrop.style.display = 'block';
+      } else {
+        unlockScroll(); 
+        modalBackdrop.style.display = 'none';
+      }
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    unlockScroll();
+    modalBackdrop.style.display = 'none';
+  });
+})();
